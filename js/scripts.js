c35af0863f0eb363ec119bdad576fc2902e740c8
Olena Kuchko and Phat Nguyen <olena+phat@>
2017-04-26 16:18:17 -0700

    practice with inputs 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,8 @@ $(function () {
       name:  $("#name").val(),
       age:   $("#age").val(),
       food:  $("input:radio[name=food]:checked").val(),
-      music: $("#music").val()
+      music: $("#music").val(),
+      color: $("#color").val()
     }
     console.log(event);
     console.log(responder);
